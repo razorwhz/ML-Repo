662be4620403e19aee84a6b2c1ef3fe6662ee5fc
razorwhz <razorwhz@163.com>
2022-10-31 15:17:47 +0800

    upload overdue files

---
+++ NN.py
@@ -0,0 +1,14 @@
+import numpy as np
+x = np.array([[1,2,3],[2,2,3],[1,1,1]])
+y = np.array([[1,1],[2,2],[3,3]])
+
+xx = np.array([1,2,3])
+yy = np.array([1,1])
+#print(np.dot(xx,yy.T))#*是各元素对应相乘，最终得到向量
+
+#print(trype(xx))
+list_a = list([0]*8)
+xx[2] = 4
+print(list_a)
+DIC = {'yy':1,'zz':2}
+print(len(DIC))
